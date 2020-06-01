39ceb47e05184236ce0ba72f05736116ce955196
Donna Torres <todonnatorres@icloud.com>
2020-06-01 05:11:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,9 +5,13 @@ end
 
 def batch_badge_creator(names)
   names.map do |name|
-    "Hello, my name is #{name}."
+    # "Hello, my name is #{name}."
+    badge_maker(name)
   end
-  
+end
+
+def
+
 end
   
 # Write your code here.
